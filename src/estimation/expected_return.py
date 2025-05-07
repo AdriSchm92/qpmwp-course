08253102e46d87f0c3311aca215e359abcfb74d5
AdriSchm92 <adrian.schmidli@uzh.ch>
2025-05-07 22:10:14 +0200

    Some changes!

--- src/estimation/expected_return.py
+++ src/estimation/expected_return.py
@@ -172,9 +172,11 @@ def mean_harmonic(X: Union[pd.DataFrame, np.ndarray],
     mu = ((n / np.sum(1 / (1 + X), axis=0)) * scalefactor) - 1
     return mu
 
-def mean_ewm(X: Union[pd.DataFrame, np.ndarray], scalefactor: Union[float, int] = 1,
+def mean_ewm(X: Union[pd.DataFrame, np.ndarray, object],
+                    scalefactor: Union[float, int] = 1,
                     span: Union[float, int] = 10, # span = 10 it is a popular choice in practice.
-                    reverse: bool = True) -> Union[pd.Series, np.ndarray]: # False = starting from the top and true = starting from the bottom.
+                    reverse: bool = True, # False = starting from the top and true = starting from the bottom.
+                    attribute: Optional[str] = None) -> Union[pd.Series, np.ndarray]: 
     """
     Calculates the exponential weighted mean (ewm) of a list of numbers.
 
@@ -182,20 +184,33 @@ def mean_ewm(X: Union[pd.DataFrame, np.ndarray], scalefactor: Union[float, int] 
     Def. scalefactor: The argument scalefactor can be either a float or an int. It has a default value of one.
     Def. span: The span parameter is a smoothing factor that determines the degree of weighting decrease. A smaller span results in more weight being given to recent observations, while a larger span gives more weight to older observations. It has a default value of ten.
     Def. reverse: If True, the function calculates the ewm starting from the bottom of the series (each column). If False, it starts from the top. This is useful for time series data where it depends where the calculation starts. It has a default value of true.
+    Def. attribute: Add explanation!!!!!!!!!!!
 
+    Adjusted it!!!!!!!!!!
     i = t, where t is the time index of the series.
     sum(weight * value) / sum(weight) = mean_ewm(X)
     weight = alpha * (1 - alpha) ** i, where alpha = 2 / (span + 1).
     value = X[i], where i is the time index of the series.
     → It is done column by column (axis=0).
+
+    Remark over the handling of the NaN!!!!!!!!!!!!!
+    Test the function in assignment 4!!!!!!!!!!!!!
     
     Data output: The function returns a pandas series or a numpy array, depending on the input type of X.    
     """
+    if not isinstance(X, pd.DataFrame):
+        if attribute is not None and hasattr(X, attribute) and isinstance(getattr(X, attribute), pd.DataFrame):
+        # 1.) "hasattr(X, attribute)" checks whether the object X has an attribute with the name stored in the variable "attribute".
+        # 2.) "isinstance(getattr(X, attribute), pd.DataFrame)" checks whether that the attribute of X is actually a pandas DataFrame, and not something else (like a string or list).
+            X = X.get_return_series()
+        else:
+            raise ValueError("If X is a BacktestData object, an 'attribute' must be specified.")
+
     denominators_dict = {}
     numerators_dict = {}
     mu_dict = {}
     mu = []
-    alpha = 2 / (span + 1) # Smoothing factor for ewm
+    alpha = 2 / (span + 1)
 
     for col in X.columns:
         series = X[col].values
@@ -206,9 +221,10 @@ def mean_ewm(X: Union[pd.DataFrame, np.ndarray], scalefactor: Union[float, int] 
         numerators = []
 
         for t in range(len(series)):
-            i = t
-            weight = alpha * (1 - alpha) ** i
-            value = series[i]
+            value = series[t]
+            if pd.isna(value):
+                continue  # "pd.isna(value)"" checks if the current value is missing (i.e., NaN, None, or pd.NA). If yes, "continue" tells Python to skip the rest of the loop body and go to the next iteration.
+            weight = alpha * (1 - alpha) ** t
             numerators.append(weight * value)
             denominators.append(weight)
 
@@ -219,7 +235,12 @@ def mean_ewm(X: Union[pd.DataFrame, np.ndarray], scalefactor: Union[float, int] 
     for col in X.columns:
         # print(f"Weights for {col}: {denominators_dict[col]}")
         # print(f"Weights * Value for {col}: {numerators_dict[col]}")
-        mu = (np.sum(numerators_dict[col]) / np.sum(denominators_dict[col])) * scalefactor
+        denom_sum = np.sum(denominators_dict[col])
+        if denom_sum == 0:
+            mu = np.nan
+        else:
+            mu = (np.sum(numerators_dict[col]) / denom_sum) * scalefactor
+
         mu_dict[col] = mu
 
     return pd.Series(mu_dict, dtype = "float64")
